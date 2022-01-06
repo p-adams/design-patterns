aa6a0509cd1d60cee0b997fc94d2ceb1eeee1a4d
p-adams <ghusiyy@gmail.com>
2022-01-05 23:02:00 -0800

    add header and improve settings layout

--- src/lib/CommandPatternEx.svelte
+++ src/lib/CommandPatternEx.svelte
@@ -13,9 +13,10 @@
   }
 </script>
 
+<header><h3>Asteroids</h3></header>
 <canvas
   on:dblclick={() => start()}
-  width={300}
+  width={500}
   height={300}
   on:keyup={(e) => onKeyup(e)}
   on:keydown={(e) => onKeyDown(e)}
@@ -23,25 +24,45 @@
 />
 <div class="control-menu-panel">
   <span>Double-click canvas to start game</span>
-  <div>
+
+  <div class="settings-controls">
     <i
       class="fas fa-gamepad"
       on:click={() => (toggleSettings = !toggleSettings)}
-    />settings
-    {#if toggleSettings}
-      <div class="menu-settings">
-        <div>controls</div>
-      </div>
-    {/if}
+    />
+    <span>Settings</span>
   </div>
+
+  {#if toggleSettings}
+    <div class="menu-settings">
+      <div>controls</div>
+    </div>
+  {/if}
 </div>
 
 <style scoped>
+  header {
+    text-align: center;
+  }
   canvas {
     border: 1px solid blue;
   }
   .control-menu-panel {
     display: flex;
     flex-direction: column;
+  }
+  .settings-controls {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+  .settings-controls i {
+    cursor: pointer;
+    font-size: 24px;
+    margin-right: 4px;
+  }
+  .settings-controls i:hover {
+    color: blue;
+    font-size: 28px;
   }
 </style>
