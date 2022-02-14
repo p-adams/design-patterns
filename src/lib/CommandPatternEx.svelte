ff89aa093e7ca8a31ae267aebbcef8c91e790a73
Paul Adams <ghusiyy@gmail.com>
2022-02-13 18:22:52 -0800

    add marked dependency to enable text editor

--- src/lib/CommandPatternEx.svelte
+++ src/lib/CommandPatternEx.svelte
@@ -1,10 +1,25 @@
 <script lang="ts">
+  import { marked } from "marked";
+  import { afterUpdate } from "svelte";
+  let input = "";
+  let markedInput = null;
+  afterUpdate(() => {
+    markedInput = marked(input, { sanitize: true });
+  });
 </script>
 
 <article>
   <!-- demonstrates command pattern with undo/redo functionality -->
-  <header>text editor</header>
-  <section />
+  <header>markdown editor</header>
+
+  <section>
+    <section class="editor">
+      <textarea bind:value={input} />
+    </section>
+    <section class="output">
+      {@html markedInput}
+    </section>
+  </section>
 </article>
 
 <style scoped>
