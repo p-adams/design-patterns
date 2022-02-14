d070060bc9fcb06765ba1aa833a24db833c9e361
Paul Adams <ghusiyy@gmail.com>
2022-02-13 18:32:17 -0800

    render textarea and html output in grid

--- src/lib/CommandPatternEx.svelte
+++ src/lib/CommandPatternEx.svelte
@@ -8,22 +8,41 @@
   });
 </script>
 
-<article>
+<article class="command-pattern-example-wrapper">
   <!-- demonstrates command pattern with undo/redo functionality -->
   <header>markdown editor</header>
 
-  <section>
-    <section class="editor">
-      <textarea bind:value={input} />
-    </section>
-    <section class="output">
-      {@html markedInput}
-    </section>
+  <section class="markdown-editor-wrapper">
+    <div class="input-output-container">
+      <div class="editor">
+        <textarea bind:value={input} />
+      </div>
+      <div class="output">
+        {@html markedInput}
+      </div>
+    </div>
   </section>
 </article>
 
 <style scoped>
+  .command-pattern-example-wrapper {
+    display: flex;
+    flex-direction: column;
+  }
+
   header {
+    text-transform: capitalize;
     text-align: center;
+  }
+  .markdown-editor-wrapper {
+    margin-top: 10px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    align-content: center;
+  }
+  .input-output-container {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
   }
 </style>
