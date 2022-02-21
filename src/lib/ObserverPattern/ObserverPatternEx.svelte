e0f76dc8651173ec4937c1c75d87af70a32e53b9
Paul Adams <ghusiyy@gmail.com>
2022-02-21 14:39:06 -0800

    provide project outline

--- src/lib/ObserverPattern/ObserverPatternEx.svelte
+++ src/lib/ObserverPattern/ObserverPatternEx.svelte
@@ -1,9 +1,44 @@
 <script lang="ts">
+  let today = new Date().toLocaleString();
+  let subscriptions: Subscription[] = [
+    { name: "announcements" },
+    { name: "school cancellations" },
+    { name: "meals" },
+  ];
 </script>
 
 <article>
-  <header>observer pattern</header>
+  <header>
+    <h2>Bedrock High School Bulletin</h2>
+    <div>Today {today}</div>
+  </header>
   <section>
-    <p>subscribe to <!--<subscription> --></p>
+    <p>Stay informed by subscribing below</p>
+    <ul>
+      {#each subscriptions as subscription}
+        <li>{subscription.name}</li>
+      {/each}
+    </ul>
   </section>
 </article>
+
+<style scoped>
+  section {
+    margin-top: 10px;
+  }
+  section p {
+    text-transform: uppercase;
+  }
+  ul {
+    list-style: none;
+    padding: 0;
+    margin: 10px auto;
+  }
+  ul li {
+    text-transform: capitalize;
+  }
+  header h2 {
+    font-size: 18px;
+    color: blue;
+  }
+</style>
---
+++ src/lib/ObserverPattern/types.d.ts
@@ -0,0 +1,3 @@
+interface Subscription {
+  name: string;
+}
