ec49d90c826e3228507a619afb3b7366658b0029
Paul Adams <ghusiyy@gmail.com>
2022-02-25 21:31:56 -0800

    create Subject class

---
+++ src/lib/ObserverPattern/numberList.ts
@@ -0,0 +1,25 @@
+type SubscriptionType = "ADD";
+
+interface Subscription {
+  type: SubscriptionType;
+  label: string;
+}
+
+interface Observer {
+  onNotify: (subscription: Subscription, type: SubscriptionType) => void;
+}
+
+export class Subject {
+  observers: Observer[] = [];
+  public addObserver(observer: Observer) {
+    this.observers.push(observer);
+  }
+  public removeObserver(observer: Observer) {
+    const index = this.observers.findIndex(
+      (_observer) => _observer === observer
+    );
+    if (index !== -1) {
+      this.observers.splice(index, 1);
+    }
+  }
+}
